11322d4134e98cfe0a96056e6dbcb6a0b4b30de1
oz-ada <oskar.zuk@ada.ac.uk>
2019-11-06 12:48:10 +0000

    a

--- U14 Game/script.js
+++ U14 Game/script.js
@@ -6,12 +6,17 @@ var plat2;
 var plat3;
 var plat4;
 var plat5;
+var GRAVITY = 1;
+var JUMP = 15;
 
+var p1jump = false
+var p2jump = false
 
 
 function preload() {
     alien = loadImage("Player1.png")
     clown = loadImage("Player2.png")
+    gun = loadImage("gun.png")
 
 }
 
@@ -25,11 +30,11 @@ function setup() {
     player2.addImage(clown);
     player2.scale = 0.3;
     players = Group();
-    
+
     ground = createSprite(500, 745, 1000, 50)
-    
+
     platforms = Group();
-    plat1 = createSprite(125,650,50,10)
+    plat1 = createSprite(125, 650, 50, 10)
     platforms.add(plat1)
 
 
@@ -39,6 +44,8 @@ function setup() {
 function draw() {
     background(61, 82, 99);
     //player 1 movement
+
+    player1.velocity.y += GRAVITY;
     if (keyIsDown(65)) {
         player1.position.x -= 5
     }
@@ -47,19 +54,12 @@ function draw() {
         player1.position.x += 5
     }
 
-    if (keyIsDown(87)) {
-        player1.position.y -= 10
-    }
-
-    if (player1.position.y < 630) { //acts as a force of gravity that pulls the player down if they go above a certain y level
-        player1.setVelocity(0, 5)
-    }
-
-    if (player1.position.y == 690) { //resets the players velocity when they hit the ground so that they can jump again
-        player1.setVelocity(0, 0)
+    if (player1.collide(ground) || player1.collide(platforms)) {
+        player1.velocity.y = 0
     }
 
     //player 2 movement
+    player2.velocity.y += GRAVITY
     if (keyIsDown(37)) {
         player2.position.x -= 5
     }
@@ -68,20 +68,39 @@ function draw() {
         player2.position.x += 5
     }
 
-    if (keyIsDown(38)) {
-        player2.position.y -= 10
+    if (player2.collide(ground) || player2.collide(platforms)) {
+        player2.velocity.y = 0
     }
 
-    if (player2.position.y < 630) {
-        player2.setVelocity(0, 5)
-    }
 
-    if (player2.position.y == 690) {
-        player2.setVelocity(0, 0)
-    }
     
-    players.collide(ground);
-    players.collide(platforms);
+    player1.collide(platforms);
+    player2.collide(platforms);
 
     drawSprites();
+}
+
+function keyPressed() {
+    if (keyCode === UP_ARROW && p2jump == false) {
+        player2.velocity.y -= JUMP;
+        p2jump = true
+    }
+    
+    else {
+        if (player2.velocity.y === 0) {
+         p2jump = false;   
+        }
+    }
+    
+    if (keyIsDown(87) && p1jump == false){
+        player1.velocity.y -= JUMP;
+        p1jump = true
+    }
+    
+    else{
+        if (player1.velocity.y === 0) {
+         p1jump = false;   
+        }
+    }
+
 }
