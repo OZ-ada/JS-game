37b8b485878eaf960df0b1c5ec76b3b23f911845
OZ-ada <oskar.zuk@ada.ac.uk>
2019-11-06 00:12:47 +0000

    added movement

--- U14 Game/script.js
+++ U14 Game/script.js
@@ -1,7 +1,11 @@
 var player1;
 var player2;
 var ground;
-var GRAVITY = 1;
+var plat1;
+var plat2;
+var plat3;
+var plat4;
+var plat5;
 
 
 
@@ -12,15 +16,21 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(500, 500)
+    createCanvas(1000, 750)
 
-    player1 = createSprite(200, 440);
+    player1 = createSprite(50, 690);
     player1.addImage(alien);
-    player1.scale = 0.3
-    player2 = createSprite(50, 440);
+    player1.scale = 0.3;
+    player2 = createSprite(950, 690);
     player2.addImage(clown);
-    player2.scale = 0.3
-    ground = createSprite(500, 495, 1000, 50)
+    player2.scale = 0.3;
+    players = Group();
+    
+    ground = createSprite(500, 745, 1000, 50)
+    
+    platforms = Group();
+    plat1 = createSprite(125,650,50,10)
+    platforms.add(plat1)
 
 
 
@@ -28,12 +38,50 @@ function setup() {
 
 function draw() {
     background(61, 82, 99);
-    
-    if (keyIsDown(65)){
-        
+    //player 1 movement
+    if (keyIsDown(65)) {
+        player1.position.x -= 5
     }
-        
+
+    if (keyIsDown(68)) {
+        player1.position.x += 5
+    }
+
+    if (keyIsDown(87)) {
+        player1.position.y -= 10
+    }
+
+    if (player1.position.y < 630) { //acts as a force of gravity that pulls the player down if they go above a certain y level
+        player1.setVelocity(0, 5)
+    }
+
+    if (player1.position.y == 690) { //resets the players velocity when they hit the ground so that they can jump again
+        player1.setVelocity(0, 0)
+    }
+
+    //player 2 movement
+    if (keyIsDown(37)) {
+        player2.position.x -= 5
+    }
+
+    if (keyIsDown(39)) {
+        player2.position.x += 5
+    }
+
+    if (keyIsDown(38)) {
+        player2.position.y -= 10
+    }
+
+    if (player2.position.y < 630) {
+        player2.setVelocity(0, 5)
+    }
+
+    if (player2.position.y == 690) {
+        player2.setVelocity(0, 0)
+    }
     
+    players.collide(ground);
+    players.collide(platforms);
 
     drawSprites();
 }
