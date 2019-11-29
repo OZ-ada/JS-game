e2c59db44eb6100185ef8c93b8c2ab4de83d4c09
OZ-ada <oskar.zuk@ada.ac.uk>
2019-11-29 01:57:01 +0000

    Made a main menu and added a background

--- U14 Game/script.js
+++ U14 Game/script.js
@@ -32,7 +32,12 @@ var p1Win = false
 var p2Win = false
 var tie = false
 
+var buttonY = 300
+
+var gameStart = false
+
 function preload() {
+    backdrop = loadImage("Assets/landscape.png")
     alien = loadImage("Assets/Player1.png")
     clown = loadImage("Assets/Player2.png")
     gun = loadImage("Assets/gun.png")
@@ -40,12 +45,46 @@ function preload() {
     heart1 = loadImage("Assets/p1Heart.png")
     heart2 = loadImage("Assets/p2Heart.png")
     jumpSound = loadSound("Assets/SFX_Jump_09.wav")
-
 }
 
 function setup() {
     createCanvas(1000, 750)
+    menuButtons = new Group();
 
+    for (var i = 0; i < 2; i++) {
+        button = createSprite(500, buttonY, 200, 150)
+        button.mouseActive = true
+        button.shapeColor = "#087874"
+        menuButtons.add(button)
+        buttonY += 200
+    }
+
+}
+
+function menu() {
+    if (menuButtons[0].visible) {
+        textAlign(CENTER)
+        textSize(250)
+        textFont("Odibee Sans")
+        text("Arena Brawl", 500, 200)
+        textAlign(CENTER)
+        textSize(50)
+        textFont("Bebas Neue")
+        fill(255)
+        text("Play", 500, 310)
+        if (menuButtons[0].mouseIsOver) {
+            if (mouseIsPressed) {
+                startup()
+                gameStart = true
+                for (var x = 0; x < 2; x++) {
+                    menuButtons[x].visible = false
+                }
+            }
+        }
+    }
+}
+
+function startup() {
     player1 = createSprite(50, 690);
     player1.addImage(alien);
     player1.scale = 0.3;
@@ -58,8 +97,11 @@ function setup() {
 
     ground = createSprite(500, 745, 1000, 50);
     leftwall = createSprite(0, 500, 10, 1000);
+    leftwall.visible = false
     rightwall = createSprite(1000, 0, 10, 10000);
+    rightwall.visible = false
     roof = createSprite(500, 0, 1000, 10);
+    roof.visible = false
     borders = new Group();
     borders.add(ground);
     borders.add(leftwall);
@@ -79,12 +121,12 @@ function setup() {
     platforms.add(plat4);
     platforms.add(plat5);
     platforms.add(plat6);
-    plat1.shapeColor = ('red');
-    plat2.shapeColor = ('red');
-    plat3.shapeColor = ('red');
-    plat4.shapeColor = ('red');
-    plat5.shapeColor = ('red');
-    plat6.shapeColor = ('red');
+    plat1.shapeColor = "#087874";
+    plat2.shapeColor = "#087874";
+    plat3.shapeColor = "#087874";
+    plat4.shapeColor = "#087874";
+    plat5.shapeColor = "#087874";
+    plat6.shapeColor = "#087874";
 
     platborder1 = createSprite(100, 290, 10, 50)
     platborder2 = createSprite(900, 290, 10, 50)
@@ -128,215 +170,226 @@ function setup() {
     p2Heart3.scale = 0.8;
 }
 
-function draw() {
-    background(61, 82, 99);
+function game() {
 
-    //player 1 movement
+    if (gameStart) {
 
-    player1.velocity.y += GRAVITY;
-    if (keyIsDown(65)) {
-        player1.position.x -= 5;
-        p1Gun.position.x = player1.position.x - 50
-        p1Gun.mirrorX(1)
-        p1Left = true
-    }
-
-    if (keyIsDown(68)) {
-        player1.position.x += 5;
-        p1Gun.position.x = player1.position.x + 50
-        p1Gun.mirrorX(-1)
-        p1Left = false
-    }
-
-    if (player1.collide(ground) || player1.collide(platforms)) {
-        player1.velocity.y = 0;
-    }
-
-    p1Gun.position.y = player1.position.y
-
-    //player 2 movement
-    player2.velocity.y += GRAVITY
-    if (keyIsDown(37)) {
-        player2.position.x -= 5;
-        p2Gun.position.x = player2.position.x - 50
-        p2Gun.mirrorX(1)
-        p2Left = true
-    }
-
-    if (keyIsDown(39)) {
-        player2.position.x += 5;
-        p2Gun.position.x = player2.position.x + 50;
-        p2Gun.mirrorX(-1);
-        p2Left = false
-    }
-
-    p2Gun.position.y = player2.position.y
-
-
-    //collisions
-    if (player2.collide(ground) || player2.collide(platforms)) {
-        player2.velocity.y = 0;
-    }
-
-    players.collide(platforms);
-    players.collide(borders);
-    borders.collide(players);
-
-    plat6.velocity.x = platspeed;
-    if (plat6.overlap(platborder2)) {
-        platspeed = -3
-    }
-    if (plat6.overlap(platborder1)) {
-        platspeed = 3
-    }
-
-    x = random(50)
-
-    if (plat4.visible == true) {
-        count++
-        if (count > 300) {
-            count = 0
+        player1.velocity.y += GRAVITY;
+        if (keyIsDown(65)) {
+            player1.position.x -= 5;
+            p1Gun.position.x = player1.position.x - 50
+            p1Gun.mirrorX(1)
+            p1Left = true
         }
-        if (count == Math.round(x)) {
-            plat4.visible = false
-            plat4.position.x = 5000;
-        } else {
+
+        if (keyIsDown(68)) {
+            player1.position.x += 5;
+            p1Gun.position.x = player1.position.x + 50
+            p1Gun.mirrorX(-1)
+            p1Left = false
+        }
+
+        if (player1.collide(ground) || player1.collide(platforms)) {
+            player1.velocity.y = 0;
+        }
+
+        p1Gun.position.y = player1.position.y
+
+        //player 2 movement
+        player2.velocity.y += GRAVITY
+        if (keyIsDown(37)) {
+            player2.position.x -= 5;
+            p2Gun.position.x = player2.position.x - 50
+            p2Gun.mirrorX(1)
+            p2Left = true
+        }
+
+        if (keyIsDown(39)) {
+            player2.position.x += 5;
+            p2Gun.position.x = player2.position.x + 50;
+            p2Gun.mirrorX(-1);
+            p2Left = false
+        }
+
+        p2Gun.position.y = player2.position.y
+
+
+        //collisions
+        if (player2.collide(ground) || player2.collide(platforms)) {
+            player2.velocity.y = 0;
+        }
+
+        players.collide(platforms);
+        players.collide(borders);
+        borders.collide(players);
+
+        plat6.velocity.x = platspeed;
+        if (plat6.overlap(platborder2)) {
+            platspeed = -3
+        }
+        if (plat6.overlap(platborder1)) {
+            platspeed = 3
+        }
+
+        x = random(50)
+
+        if (plat4.visible == true) {
             count++
-            if (count == 300) {
-                plat4.visible = true
+            if (count > 300) {
+                count = 0
+            }
+            if (count == Math.round(x)) {
+                plat4.visible = false
+                plat4.position.x = 5000;
+            } else {
+                count++
+                if (count == 300) {
+                    plat4.visible = true
+                }
             }
         }
-    }
 
-    if (plat4.visible == false) {
-        if (count < 200) {
-            count++
-        }
-        if (count == 200) {
-            plat4.position.x = 200;
-            plat4.visible = true;
-        }
-    }
-
-    //Player 1 Shooting
-    if (keyIsDown(32) && p1Left == true && p1Shooting == false) {
-        p1Shooting = true
-        p1Projectile.position.x = p1Gun.position.x
-        p1Projectile.position.y = player1.position.y - 10
-        p1Projectile.visible = true
-        p1Projectile.velocity.x = -20
-    }
-
-    if (keyIsDown(32) && p1Left == false && p1Shooting == false) {
-        p1Shooting = true
-        p1Projectile.position.x = p1Gun.position.x
-        p1Projectile.position.y = player1.position.y - 10
-        p1Projectile.visible = true
-        p1Projectile.velocity.x = 20
-    }
-
-    if (p1Shooting){
-        p1Cooldown ++
-    }
-    if(p1Cooldown == 45){
-        p1Cooldown = 0
-        p1Shooting = false
-    }
-
-    //Player 2 Shooting
-    if (keyIsDown(45) && p2Left == true && p2Shooting == false) {
-        p2Shooting = true
-        p2Projectile.position.x = p2Gun.position.x
-        p2Projectile.position.y = player2.position.y - 10
-        p2Projectile.visible = true
-        p2Projectile.velocity.x = -20
-    }
-
-    if (keyIsDown(45) && p2Left == false && p2Shooting == false) {
-        p2Shooting = true
-        p2Projectile.position.x = p2Gun.position.x
-        p2Projectile.position.y = player2.position.y - 10
-        p2Projectile.visible = true
-        p2Projectile.velocity.x = 20
-    }
-    
-    if (p2Shooting){
-        p2Cooldown ++
-    }
-    if(p2Cooldown == 45){
-        p2Cooldown = 0
-        p2Shooting = false
-    }    
-    
-    //p1 damage
-    if (p2Projectile.overlap(player1)) {
-        p1Life -= 1
-        p2Projectile.position.x = 5000
-        p2Projectile.velocity.x = 0
-
-    }
-    //p2 damage
-    if (p1Projectile.overlap(player2)) {
-        p2Life -= 1
-        p1Projectile.position.x = 5000
-        p1Projectile.velocity.x = 0
-    }
-    
-    if(p1Life < 3){
-        p1Heart3.visible = false
-        if (p1Life < 2){
-            p1Heart2.visible = false
-            if(p1Life < 1){
-                p1Heart1.visible = false
-                p2Win = true
-                player1.position.x = 5000
+        if (plat4.visible == false) {
+            if (count < 200) {
+                count++
+            }
+            if (count == 200) {
+                plat4.position.x = 200;
+                plat4.visible = true;
             }
         }
-    }
-    
-    if(p2Life < 3){
-        p2Heart1.visible = false
-        if(p2Life < 2){
-            p2Heart2.visible = false
-            if(p2Life < 1){
-                p2Heart3.visible = false
-                p1Win = true
-                player2.position.x = 5000
+
+        //Player 1 Shooting
+        if (keyIsDown(32) && p1Left == true && p1Shooting == false) {
+            p1Shooting = true
+            p1Projectile.position.x = p1Gun.position.x
+            p1Projectile.position.y = player1.position.y - 10
+            p1Projectile.visible = true
+            p1Projectile.velocity.x = -20
+        }
+
+        if (keyIsDown(32) && p1Left == false && p1Shooting == false) {
+            p1Shooting = true
+            p1Projectile.position.x = p1Gun.position.x
+            p1Projectile.position.y = player1.position.y - 10
+            p1Projectile.visible = true
+            p1Projectile.velocity.x = 20
+        }
+
+        if (p1Shooting) {
+            p1Cooldown++
+        }
+        if (p1Cooldown == 45) {
+            p1Cooldown = 0
+            p1Shooting = false
+        }
+
+        //Player 2 Shooting
+        if (keyIsDown(45) && p2Left == true && p2Shooting == false) {
+            p2Shooting = true
+            p2Projectile.position.x = p2Gun.position.x
+            p2Projectile.position.y = player2.position.y - 10
+            p2Projectile.visible = true
+            p2Projectile.velocity.x = -20
+        }
+
+        if (keyIsDown(45) && p2Left == false && p2Shooting == false) {
+            p2Shooting = true
+            p2Projectile.position.x = p2Gun.position.x
+            p2Projectile.position.y = player2.position.y - 10
+            p2Projectile.visible = true
+            p2Projectile.velocity.x = 20
+        }
+
+        if (p2Shooting) {
+            p2Cooldown++
+        }
+        if (p2Cooldown == 45) {
+            p2Cooldown = 0
+            p2Shooting = false
+        }
+
+        //p1 damage
+        if (p2Projectile.overlap(player1)) {
+            p1Life -= 1
+            p2Projectile.position.x = 5000
+            p2Projectile.velocity.x = 0
+
+        }
+        //p2 damage
+        if (p1Projectile.overlap(player2)) {
+            p2Life -= 1
+            p1Projectile.position.x = 5000
+            p1Projectile.velocity.x = 0
+        }
+
+        if (p1Life < 3) {
+            p1Heart3.visible = false
+            if (p1Life < 2) {
+                p1Heart2.visible = false
+                if (p1Life < 1) {
+                    p1Heart1.visible = false
+                    p2Win = true
+                    player1.position.x = 5000
+                }
             }
         }
-    }
-    
-    if(p1Win == true && p2Win == true){
-        tie = true
-    }
-    
-    text(p1Life, 500, 500)
-    text(p2Life, 600, 500)
 
+        if (p2Life < 3) {
+            p2Heart1.visible = false
+            if (p2Life < 2) {
+                p2Heart2.visible = false
+                if (p2Life < 1) {
+                    p2Heart3.visible = false
+                    p1Win = true
+                    player2.position.x = 5000
+                }
+            }
+        }
 
-    drawSprites();
+        if (p1Win == true && p2Win == true) {
+            tie = true
+        }
+
+        text(p1Life, 500, 500)
+        text(p2Life, 600, 500)
+    }
 
 }
 
+function draw() {
+
+    background(backdrop);
+
+    game()
+
+    drawSprites();
+
+    menu()
+
+}
 
 function keyPressed() {
-    if (keyCode === UP_ARROW && p2jump == false) {
-        player2.velocity.y -= JUMP;
-        p2jump = true
-        jumpSound.play()
-    } else {
-        if (player2.velocity.y === 0) {
-            p2jump = false;
-        }
-    }
+    if (gameStart) {
 
-    if (keyIsDown(87) && p1jump == false) {
-        player1.velocity.y -= JUMP;
-        p1jump = true
-        jumpSound.play()
-    } else {
-        if (player1.velocity.y === 0) {
-            p1jump = false;
+        if (keyCode === UP_ARROW && p2jump == false) {
+            player2.velocity.y -= JUMP;
+            p2jump = true
+            jumpSound.play()
+        } else {
+            if (player2.velocity.y === 0) {
+                p2jump = false;
+            }
+        }
+
+        if (keyIsDown(87) && p1jump == false) {
+            player1.velocity.y -= JUMP;
+            p1jump = true
+            jumpSound.play()
+        } else {
+            if (player1.velocity.y === 0) {
+                p1jump = false;
+            }
         }
     }
 }
